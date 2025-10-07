8cb1fed8ef0c7bdd5d354b9acb03999175963b0c
IvanGLO <105451370+IvanGLO@users.noreply.github.com>
2025-10-07 12:40:32 -0700

    actualizando version

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,52 @@
-function cambiarPlayera(nombre) {
+function cambiarPlayera(nombre, elemento) {
+  // Cambia la imagen
   document.getElementById("playera").src = "assets/playeras/" + nombre;
+
+  // Quita la clase "selected" de todos los elementos similares
+  document.querySelectorAll(".color").forEach(el => {
+    el.classList.remove("selected");
+  });
+
+  // Agrega la clase "selected" al elemento que llamó la función
+  elemento.classList.add("selected");
 }
 
-function colocarDiseno(nombre) {
-  document.getElementById("diseno_img").src = "assets/disenos/" + nombre;
+function cambiarTalla(talla, elemento) {
+  // Cambia la imagen
+  document.getElementById("lbl-sel-talla").textContent = "Talla: " + talla;
+
+  // Quita la clase "selected" de todos los elementos similares
+  document.querySelectorAll(".talla").forEach(el => {
+    el.classList.remove("selected");
+  });
+
+  // Agrega la clase "selected" al elemento que llamó la función
+  elemento.classList.add("selected");
+}
+
+function colocarDiseno(nombre, elemento) {
+  //Cambiamos el diseño en el preview
+
+  document.getElementById("diseno_img").src = elemento.src;
+  document.getElementById("lbl-sel-img").textContent = "Dibujo: " + nombre.replace(".png","");
+
+  document.querySelectorAll(".diseno").forEach(el => {
+    el.classList.remove("selected");
+  });
+
+  // Agrega la clase "selected" al elemento que llamó la función
+  elemento.classList.add("selected");
+}
+
+function cambiarCategoria(cat, elemento) {
+  //Cambiamos el diseño en el preview
+
+  document.querySelectorAll(".cat").forEach(el => {
+    el.classList.remove("selected");
+  });
+
+  // Agrega la clase "selected" al elemento que llamó la función
+  elemento.classList.add("selected");
 }
 
 function moverCarrusel(direccion) {
